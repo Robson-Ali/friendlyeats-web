516968151f381650aa03d01b68981d46ecc0ea72
Robson-Ali <99282807+Robson-Ali@users.noreply.github.com>
2025-12-04 10:07:56 +0300

    Displaying the list of restaurants

--- src/lib/firebase/storage.js
+++ src/lib/firebase/storage.js
@@ -4,8 +4,35 @@ import { storage } from "@/src/lib/firebase/clientApp";
 
 import { updateRestaurantImageReference } from "@/src/lib/firebase/firestore";
 
-// Replace the two functions below
-export async function updateRestaurantImage(restaurantId, image) {}
+function applyQueryFilters(q, { category, city, price, sort }) {
+  if (category) {
+    q = query(q, where("category", "==", category));
+  }
+  if (city) {
+    q = query(q, where("city", "==", city));
+  }
+  if (price) {
+    q = query(q, where("price", "==", price.length));
+  }
+  if (sort === "Rating" || !sort) {
+    q = query(q, orderBy("avgRating", "desc"));
+  } else if (sort === "Review") {
+    q = query(q, orderBy("numRatings", "desc"));
+  }
+  return q;
+}
 
-async function uploadImage(restaurantId, image) {}
-// Replace the two functions above
+export async function getRestaurants(db = db, filters = {}) {
+  let q = query(collection(db, "restaurants"));
+
+  q = applyQueryFilters(q, filters);
+  const results = await getDocs(q);
+  return results.docs.map((doc) => {
+    return {
+      id: doc.id,
+      ...doc.data(),
+      // Only plain objects can be passed to Client Components from Server Components
+      timestamp: doc.data().timestamp.toDate(),
+    };
+  });
+}
